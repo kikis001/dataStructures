2fb5776e19a65ae6405b4d6f645598d62dfa5ea8
Luis <kikisxx.150@gmail.com>
2024-08-29 21:34:39 -0600

    Pendientes: 9, 10

--- src/Fes/Aragon/Dinamicos/ListaSimple.java
+++ src/Fes/Aragon/Dinamicos/ListaSimple.java
@@ -15,6 +15,57 @@ public class ListaSimple<E> {
         length = 1;
     }
 
+    public int getLongitud(){
+        return this.length;
+    }
+
+    public boolean esVacia() {
+        if(length == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public ListaSimple<E> eliminarEnCabeza() {
+        if(length == 1) {
+           head = null;
+           tail = null;
+       }else {
+           head = head.next;
+       }
+       length--;
+       return this;
+    }
+
+    public ListaSimple<E> eliminarEnCola() {
+        if(length == 0 ) {
+            return this;
+        }else if(length == 1) {
+            head = null;
+            tail = null;
+        }else {
+            Nodo<E> penultimo = currentIndex(length - 2);
+            penultimo.next = null;
+            tail = penultimo;
+        }
+        length--;
+        return this;
+    }
+
+    public E obtenerNodo(int i) {
+        return currentIndex(i-1).value;
+    }
+
+    public int estaEnLista(E x) {
+        Nodo<E> currentNodo = head;
+        int counter = 0;
+        while(currentNodo.value != x) {
+            counter++;
+            currentNodo =  currentNodo.next;
+        }
+        return counter;
+    }
+
     /**
      * Método que añade un nuevo nodo con el valor especificado al final de la lista.
      * @param value El valor del nuevo nodo.
@@ -67,6 +118,9 @@ public class ListaSimple<E> {
      * @return El nodo en el índice especificado.
      */
     private Nodo<E> currentIndex(int i) {
+        if (i < 0 || i >= length) {
+            throw new IndexOutOfBoundsException("Índice fuera de rango: " + i);
+        }
         int counter = 0;
         Nodo<E> current = head;
         while(counter != i) {
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,9 @@ public class Main {
     public static void main(String[] args) {
         ListaSimple<Integer> list = new ListaSimple<>(4);
         list.append(12).append(14).append(52).print();
+        //list.print();
 
-        list.remove(2).print();
+        list.eliminarEnCola().print();
+        System.out.println(list.estaEnLista(14));
     }
 }
